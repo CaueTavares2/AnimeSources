f2915619a590f53674d4f10dcf624a5525a5c458
CaueTavares2 <141565961+CaueTavares2@users.noreply.github.com>
2025-11-23 20:25:30 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,30 @@
+plugins {
+    kotlin("jvm") version "1.9.10"
+}
 
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jsoup:jsoup:1.16.1")
+}
+
+sourceSets {
+    main {
+        java.srcDirs("src/main/kotlin")
+    }
+}
+
+tasks.register<Jar>("assembleRelease") {
+    archiveBaseName.set("AnimeSources")
+    archiveVersion.set("1.0.0")
+    from(sourceSets.main.get().output)
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    manifest {
+        attributes["Extension"] = "true"
+        attributes["Extension-Name"] = "AnimeSources"
+        attributes["Extension-Version"] = "1.0.0"
+        attributes["Extension-Pkg"] = "com.animesources.animiru"
+    }
+}
