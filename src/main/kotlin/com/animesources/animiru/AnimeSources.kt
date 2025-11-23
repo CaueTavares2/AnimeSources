f34abbdbb3694c21eb2bb2c5c7d5e8d14dc4a724
CaueTavares2 <141565961+CaueTavares2@users.noreply.github.com>
2025-11-23 19:57:08 -0300

    Update AnimeSources.kt

--- src/main/kotlin/com/animesources/animiru/AnimeSources.kt
+++ src/main/kotlin/com/animesources/animiru/AnimeSources.kt
@@ -1,1 +1,12 @@
+package com.animesources.animiru
 
+import eu.kanade.tachiyomi.source.SourceFactory
+import com.animesources.animiru.betteranime.BetterAnime
+import com.animesources.animiru.animesonlinecc.AnimesOnlineCC
+
+class AnimeSources : SourceFactory {
+    override fun createSources() = listOf(
+        BetterAnime(),
+        AnimesOnlineCC()
+    )
+}
