12929d6ae7ccdf4da3d6e2fd156847129f1356bb
CaueTavares2 <141565961+CaueTavares2@users.noreply.github.com>
2025-11-23 20:27:25 -0300

    Update BetterAnime.kt

--- src/main/kotlin/com/animesources/animiru/betteranime/BetterAnime.kt
+++ src/main/kotlin/com/animesources/animiru/betteranime/BetterAnime.kt
@@ -1,1 +1,10 @@
+package com.animesources.animiru.betteranime
 
+import eu.kanade.tachiyomi.anime.source.AnimeHttpSource
+
+class BetterAnime : AnimeHttpSource() {
+    override val name = "BetterAnime"
+    override val baseUrl = "https://betteranime.net"
+    override val lang = "pt-BR"
+    override val supportsLatest = true
+}
