e7cb2f70969b6578edec408b615edbec52ac95d1
CaueTavares2 <141565961+CaueTavares2@users.noreply.github.com>
2025-11-23 20:27:44 -0300

    Update AnimesOnlineCC.kt

--- src/main/kotlin/com/animesources/animiru/animesonlinecc/AnimesOnlineCC.kt
+++ src/main/kotlin/com/animesources/animiru/animesonlinecc/AnimesOnlineCC.kt
@@ -1,1 +1,10 @@
+package com.animesources.animiru.animesonlinecc
 
+import eu.kanade.tachiyomi.anime.source.AnimeHttpSource
+
+class AnimesOnlineCC : AnimeHttpSource() {
+    override val name = "AnimesOnlineCC"
+    override val baseUrl = "https://animesonlinecc.to"
+    override val lang = "pt-BR"
+    override val supportsLatest = true
+}
